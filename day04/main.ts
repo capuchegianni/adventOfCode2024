5d522e900d0d5817e7efc8a9febb9f664e4c10a9
Gianni Henriques <gianni.henriques@gmail.com>
2024-12-04 09:54:32 +0100

    feat(day04): task02

--- day04/main.ts
+++ day04/main.ts
@@ -34,4 +34,34 @@ const task01 = () => {
     console.log(res)
 }
 
+const task02 = () => {
+    const data = readfile('./input').split('\r\n')
+    let res = 0
+
+    data.forEach((line, x) => {
+        line.split('').forEach((char, y) => {
+            if (char === 'A') {
+                let leftDiagonal = ['M', 'S']
+                let rightDiagonal = ['M', 'S']
+
+                if (!data[x + 1] || !data[x - 1] || !data[x][y + 1] || !data[x][y - 1])
+                    return 0
+                if (leftDiagonal.includes(data[x - 1][y - 1]))
+                    leftDiagonal = leftDiagonal.filter(char => char !== data[x - 1][y - 1])
+                if (rightDiagonal.includes(data[x - 1][y + 1]))
+                    rightDiagonal = rightDiagonal.filter(char => char !== data[x - 1][y + 1])
+                if (rightDiagonal.includes(data[x + 1][y - 1]))
+                    rightDiagonal = rightDiagonal.filter(char => char !== data[x + 1][y - 1])
+                if (leftDiagonal.includes(data[x + 1][y + 1]))
+                    leftDiagonal = leftDiagonal.filter(char => char !== data[x + 1][y + 1])
+                if (!leftDiagonal.length && !rightDiagonal.length)
+                    res++
+            }
+        })
+    })
+
+    console.log(res)
+}
+
 task01()
+task02()
